8e33a5a69ea79bac15303dfd2a0e3b3ad7ed3d7a
altayar <mahammed_altayar@hotmail.com>
2013-07-10 03:32:15 +0300

    Revert "Change the path inside the scripts,"
    This reverts commit e78ba2b16f5a5f0d7b186a3835ea38315ca83696.

--- FILECARVE_ME01t/add.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#sed '/6576/a cat h2txt/6576.txt >> h2txt/$header.txt' catchfooter.sh
-sed -i "/$1/a cat h2txt/$2.txt >> h2txt/\$header.txt" ~/projects/carving/FILECARVE_ME01/catchfooter.sh
